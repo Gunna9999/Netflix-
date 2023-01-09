a3c2f9b8ca2a06d355e16d5212a292914182dc24
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-01-09 11:23:07 +0100

    COMMIT

---
+++ script.js
@@ -0,0 +1,115 @@
+const plus1 = document.querySelector("#plus1")
+const mark1 = document.querySelector("#mark1");
+
+plus1.addEventListener ("click" , () => {
+    document.querySelector("#plus1").style.display = "none";
+    document.querySelector("#plus1").style.marginTop = "-2rem";
+    document.querySelector("#mark1").style.display = "block";
+    document.querySelector("#mark1").style.marginTop = "-1.5rem";
+    document.querySelector("#Ans1").style.display = "block";
+})
+
+mark1.addEventListener("click", () => {
+    document.querySelector("#mark1").style.display = "none";
+    document.querySelector("#plus1").style.display = "block";
+    document.querySelector("#Ans1").style.display = "none";
+
+})
+
+
+
+
+
+
+
+const plus2 = document.querySelector("#plus2");
+const mark2 = document.querySelector("#mark2");
+
+plus2.addEventListener("click", () => {
+    document.querySelector("#plus2").style.display = "none";
+    document.querySelector("#plus2").style.marginTop = "-2rem";
+    document.querySelector("#mark2").style.display = "block";
+    document.querySelector("#mark2").style.marginTop = "-1.5rem";
+    document.querySelector("#Ans2").style.display = "block";
+})
+
+mark2.addEventListener("click", () => {
+    document.querySelector("#mark2").style.display = "none";
+    document.querySelector("#plus2").style.display = "block";
+    document.querySelector("#Ans2").style.display = "none";
+
+})
+
+
+const plus3 = document.querySelector("#plus3");
+const mark3 = document.querySelector("#mark3");
+
+plus3.addEventListener("click", () => {
+    document.querySelector("#plus3").style.display = "none";
+    document.querySelector("#plus3").style.marginTop = "-2rem";
+    document.querySelector("#mark3").style.display = "block";
+    document.querySelector("#mark3").style.marginTop = "-1.5rem";
+    document.querySelector("#Ans3").style.display = "block";
+})
+
+mark3.addEventListener("click", () => {
+    document.querySelector("#mark3").style.display = "none";
+    document.querySelector("#plus3").style.display = "block";
+    document.querySelector("#Ans3").style.display = "none";
+})
+
+
+const plus4 = document.querySelector("#plus4");
+const mark4 = document.querySelector("#mark4");
+
+plus4.addEventListener("click", () => {
+    document.querySelector("#plus4").style.display = "none";
+    document.querySelector("#plus4").style.marginTop = "-2rem";
+    document.querySelector("#mark4").style.display = "block";
+    document.querySelector("#mark4").style.marginTop = "-1.5rem";
+    document.querySelector("#Ans4").style.display = "block";
+})
+
+mark4.addEventListener("click", () => {
+    document.querySelector("#mark4").style.display = "none";
+    document.querySelector("#plus4").style.display = "block";
+    document.querySelector("#Ans4").style.display = "none";
+})
+
+
+const plus5 = document.querySelector("#plus5");
+const mark5 = document.querySelector("#mark5");
+
+plus5.addEventListener("click", () => {
+    document.querySelector("#plus5").style.display = "none";
+    document.querySelector("#plus5").style.marginTop = "-2rem";
+    document.querySelector("#mark5").style.display = "block";
+    document.querySelector("#mark5").style.marginTop = "-1.5rem";
+    document.querySelector("#Ans5").style.display = "block";
+})
+
+mark5.addEventListener("click", () => {
+    document.querySelector("#mark5").style.display = "none";
+    document.querySelector("#plus5").style.display = "block";
+    document.querySelector("#Ans5").style.display = "none";
+})
+
+
+const plus6 = document.querySelector("#plus6");
+const mark6 = document.querySelector("#mark6");
+
+plus6.addEventListener("click", () => {
+    document.querySelector("#plus6").style.display = "none";
+    document.querySelector("#plus6").style.marginTop = "-2rem";
+    document.querySelector("#mark6").style.display = "block";
+    document.querySelector("#mark6").style.marginTop = "-1.5rem";
+    document.querySelector("#Ans6").style.display = "block";
+})
+
+mark6.addEventListener("click", () => {
+    document.querySelector("#mark6").style.display = "none";
+    document.querySelector("#plus6").style.display = "block";
+    document.querySelector("#Ans6").style.display = "none";
+})
+
+
